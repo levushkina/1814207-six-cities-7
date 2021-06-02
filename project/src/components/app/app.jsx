edc66db379ff02ec689bab5a68c9ce5217824013
Levushkina Marina <nara-yan@bk.ru>
2021-06-02 22:27:07 +0500

    Начинаем программировать

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MainScreen from '../main-screen/main-screen';
 
-function App() {
-  return <p>Hello, world!</p>;
+
+function App(props) {
+  const {places, placesCount} = props;
+
+  return <MainScreen places={places} placesCount={placesCount}/>;
 }
+
+App.propTypes = {
+  places: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      rating: PropTypes.string,
+      mark: PropTypes.string,
+      price: PropTypes.string,
+      imageUrl: PropTypes.string,
+    }),
+  ).isRequired,
+  placesCount: PropTypes.number.isRequired,
+};
 
 export default App;
